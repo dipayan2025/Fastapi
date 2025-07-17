49e90494241405830485a6e1d24c72205e031ac7
dipayan2025 <er.dipayanlodh@gmail.com>
2025-07-17 18:06:31 +0530

    added

---
+++ models.py
@@ -0,0 +1,11 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+
+
+class Todos(Base):
+    __tablename__ = 'todos'
+    id = Column(Integer,primary_key=True,index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    priority = Column(String)
+    complete = Column(Boolean, default=False)
